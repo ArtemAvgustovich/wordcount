40644f55dec67453984f32b6739144c4e1315041
Artemon Avgustovich <artemon.avgustovich@semrush.com>
2023-01-15 21:46:53 +0100

    fixed empty line

--- main.go
+++ main.go
@@ -3,35 +3,26 @@ package main
 import (
 	"flag"
 	"strings"
-	"errors"
 	"fmt"
-	"os"
 )
 
 // readInput reads pattern and source string
 // from command line arguments and returns them.
 func main() {
-	src, err := readInput()
-	if err != nil {
-		fail(err)
+	src := readInput()
+	var size int
+	if src == "" {
+		size = 0
+	} else {
+		size = len(strings.Split(src, " "))
 	}
-	size := len(strings.Split(src, " "))
 	fmt.Println(size)
 }
 
 // read input from command line and return
 // concatenated string
-func readInput() (src string, err error) {
+func readInput() string {
 	flag.Parse()
-	src = strings.Join(flag.Args(), "")
-	if src == "" {
-		return src, errors.New("missing string to match")
-	}
-	return src, nil
-}
-
-// fail prints the error and exits.
-func fail(err error) {
-	fmt.Println("match:", err)
-	os.Exit(1)
+	src := strings.Join(flag.Args(), "")
+	return src
 }
